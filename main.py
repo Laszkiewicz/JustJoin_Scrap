ee49bdc4b2986a2a3ada370beeb9cb4ea2d70fcd
Laszkiewicz <laszkiewiczsebastiann@gmail.com>
2022-09-19 16:05:39 +0200

    Class_Refactor 0.3

--- main.py
+++ main.py
@@ -13,23 +13,28 @@ class BlackBoard:
         self.selenium_service = Service("D:/chromedriver.exe")
         self.driver = webdriver.Chrome(service=self.selenium_service)
         self.driver.get("https://justjoin.it/all/python/junior")
+
         self.elements = self.driver.find_elements(By.CSS_SELECTOR, "div.jss234")
         self.links = self.driver.find_elements(By.TAG_NAME, "a")
 
-    time.sleep(5)  # To let page load properly
+    @staticmethod
+    def sleep():
+        time.sleep(5)
 
     def job_search(self):
         for element in self.elements:
             self.jobs.append(element.text)
-
         for link in self.links:
             self.jobs_link.append(link.get_attribute("href"))
-        self.jobs = [i.split('\n',1)[0] for i in self.jobs]
-        self.jobs_link = self.jobs_link[8:]
+        self.jobs = [i.split('\n', 1)[0] for i in self.jobs]  # Clean out unwanted data.
+        self.jobs_link = self.jobs_link[8:21]  # First 8 links are not job related
         print(self.jobs)
         print(self.jobs_link)
+        print(len(self.jobs))
+        print(len(self.jobs_link))
 
 
 test = BlackBoard()
+test.sleep()
 test.job_search()
 
